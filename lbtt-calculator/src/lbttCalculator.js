f0fc0476beb8e83dd85d4e067ae558981612f26e
aliafarhana88 <alia.abdulghaffar@gmail.com>
2023-03-09 13:37:52 +0000

    barebone test script and function added

--- lbtt-calculator/src/lbttCalculator.js
+++ lbtt-calculator/src/lbttCalculator.js
@@ -1,3 +1,257 @@
 
-export function lbttCalculator ( propertyPrice ) { 
-    return 0 }
+// Refactor plan:
+
+//write a main function that computes LBTT for property purchase
+    // input parameters to consider:  
+        // transactionDate : Date passed as String in format "YYYY-MM-DD"
+        // propertyType : String
+        // purchasePrice :  Number   
+        // isADSapply : Boolean (initialize to false)
+        // otherPropertyPrice: Number (initialize to 0)
+        // isFirstTimeBuyer : Boolean (initialize to false)
+        // isLinkedTransaction : Boolean (initialize to false)
+    // output : {LBTT, ADS, firstTimeBuyerRelief, totalTax}
+    // think about error handling
+    
+
+    //function lbttCalculator (transactionDate, propertyType, purchasePrice, isADSapply, otherPropertyPrice, isFirstTimeBuyer) {
+        // initialize LBBT variable to 0
+
+        // if transactionDate is before 1 April 2015
+            // return -1 (LBTT not applicable)
+
+        // if propertyType is residential
+            // if transactionDate is equal or greater than 1 April 2021 OR transactionDate is less than 15 July 2020
+                
+                // get BandApril2021
+                // call LBTTResidential function 
+                // add the result to LBTT
+
+                // if firstTimeBuyer is true
+                    // call LBTTfirstTimeBuyerRelief function
+                    // subtract the result to LBTT 
+                    
+                    
+            // else if transactionDate is equals or greater than 15 july 2020 && transactionDate is less than 1 April 2021 
+                // get BandJuly2020
+                // call LBTTResidential function 
+                // add the result to LBTT
+                // set firstTimeBuyerRelief to 0
+
+            // else 
+                // return -1 (error)
+            
+
+            // if isADSapply is true
+                // call ADS function 
+                // add the result to LBTT
+
+        // else if propertyType is commercial 
+            //EDIT HERE
+            
+        // ALSO REFACTOR TO INCLUDE LINKED TRANSACTION - will affect LBTT calculation (ADS may apply)
+    //}
+
+
+//----------------------------//
+
+//write a function that computes the LBTTBand category
+    // depends on year of transaction
+
+    // function lbttBandCategory (transactionDate){
+        // if transactionDate is equal or greater than 1 April 2021 OR transactionDate is less than 15 July 2020
+            // residential property purchased on 1 April 2021 onwards: 
+            // price 145000 or less - 0% LBTT
+            // price between £145,001 to £250,000 - 2% LBTT
+            // price between  £250,001 to £325,000 - 5% LBTT
+            // price between £325,001 to £750,000 - 10% LBTT
+            // price over £750,000  - 12% LBTT
+            // return BandApril2021
+
+        // else if transactionDate is equals or greater than 15 july 2020 && transactionDate is less than  to 1 April 2021
+             // residential property purchased between 15 July 2020 to 31 March 2021 (inclusive):
+            //price £250,000 or less - 0% LBTT
+            // return BandJuly2020
+
+        // else
+            // return -1 (error)
+
+    // }
+
+//-----------------------------//
+    // function firstTimeBuyerRelief ( propertyPrice ) {
+        // residential property price £175,000 or less - 0% LBTT
+
+        //conditions:
+            //if transaction is between 15 July 2020 and 1 April 2021, first time buyer relief do not apply
+            //if ADS applies, first time buyer relief do not apply
+
+        // error handling
+    //}
+
+
+   
+
+//-----------------------------//
+
+// write a function to compute ADS
+      // input parameter : propertyPrice, transactionDate
+      // output : ADS
+        // function ADS ( propertyPrice, transactionDate ) {
+            // transaction on or after 16 December 2022 - 6% of the purchase price
+            // transaction on or after 25 January 2019 - 4% of the purchase price
+            // transaction prior to 25 January 2019 - 3% of the purchase price
+            
+            // error handling    
+                //if propertyPrice is negative
+                //if propertyPrice is string
+                //if otherPropertyPrice is greater than purchasePrice
+        // }
+
+
+//write a function to compute LBTT for residential property purchase
+export function lbttCalculator ( transactionDate, purchasePrice, isADSapply = false, otherPropertyPrice = 0, isFirstTimeBuyer = false ) { 
+    //initialize variables
+    let lbttDue = 0
+    let remainingPurchasePrice = purchasePrice
+    let firstTimeBuyerRelief = 0
+    let adsDue = 0
+    let lbttTaxTotal = 0
+
+    
+    //get the LBTT band category
+    
+    
+    //compute LBTT for residential property purchase
+    //loop through the LBTT band array
+    //compute amount of tax due in each band 
+    //keep track of remaining taxable property price 
+    //break the loop if remaining taxable property price is 0 or less
+
+
+    //write for loop
+        //exit the loop if remainingPurchasePrice is less than 0
+        
+
+        //get variables of the current band from lbtt array
+       
+        
+        //get the value of the lower limit. 
+        //exception for if index is 0, set the lower limit to 0
+       
+
+        //compute lbtt due in each band iteratively
+        //first, initialize a taxable amount variable to 0
+        //if propertyPrice is under the top limit for a band,
+            //taxable amount is equivalent to the price over the lower limit. 
+        //if properetyPrice is greater than the top limit, 
+            //taxable amount is eqivalent to the min and max threshold of the band
+            //ie: taxable amount = top limit - low limit
+       
+        
+        // compute lbtt tax in this band 
+            //lbttInBand = lbttRate*taxableAmount
+       
+
+        //add lbtt tax in this band to the lbttDue variable
+        
+
+        //substract taxedAmount from the remainingPurchasePrice
+        
+    
+
+    //if ADS apply
+
+    if(isADSapply){
+        //compute ADS due
+        adsDue = ADS(transactionDate, otherPropertyPrice)
+    }
+
+    //if first time buyer relief applies 
+    if(isFirstTimeBuyer && (transactionDate >= '2021-04-01' || transactionDate < '2020-07-15')){
+        //compute the first time buyer relief
+
+        //initialize priceLimitFTB to £175000
+
+        //if the purchasePrice is less than or equal to £175000, 
+            //the first time buyer relief is lbttDue
+        //if the purchasePrice is greater than £175000
+            // the first time buyer relief is equal to
+            //(£175000 - £145000) * 0.02
+            //OR (£175000 - lbttBand[0].priceLimit) * lbttBand[1].lbttRate
+
+
+    }
+
+    //compute total tax
+    //take account ADS and firstTimeBuyerRelief 
+    lbttTaxTotal = lbttDue + adsDue - firstTimeBuyerRelief
+
+    let lbttObject = { LBTT : lbttDue,
+                ADS : adsDue,  
+                firstTimeBuyerRelief : firstTimeBuyerRelief,
+                totalTax : lbttTaxTotal  
+    }
+    
+    //return lbttObject
+    return 0
+}
+
+
+export function lbttBandCategory(transactionDate){
+    //define LBTT bands in an array that stores the LBTT band based on the upper threshold for each band
+    const lbttBandApril2021 = [
+        {priceLimit: 145000, lbttRate: 0},
+        {priceLimit: 250000, lbttRate: 0.02},
+        {priceLimit: 325000, lbttRate: 0.05},
+        {priceLimit: 750000, lbttRate: 0.1},
+        {priceLimit: Infinity, lbttRate: 0.12}
+    ]    
+
+    const lbttBandJuly2020 = [
+        {priceLimit: 145000, lbttRate: 0},
+        {priceLimit: 250000, lbttRate: 0},
+        {priceLimit: 325000, lbttRate: 0.05},
+        {priceLimit: 750000, lbttRate: 0.1},
+        {priceLimit: Infinity, lbttRate: 0.12}
+    ]    
+
+    if (transactionDate >= '2021-04-01' || transactionDate < '2020-07-15'){
+        return lbttBandApril2021
+    }
+    else if(transactionDate >= '2020-07-15' && transactionDate < '2021-04-01'){
+        return lbttBandJuly2020
+    }else {
+        return -1
+    }
+
+    }
+
+export function ADS(transactionDate, otherPropertyPrice){
+    //initialize ADSdue variable
+    let ADSdue = 0
+    if (transactionDate >= '2022-12-16'){
+        ADSdue = 0.06*otherPropertyPrice
+    }else if(transactionDate >= `2019-01-25`){
+        ADSdue = 0.04*otherPropertyPrice
+    }else if(transactionDate < `2019-01-25`){
+        ADSdue = 0.03*otherPropertyPrice
+    }else{
+        return -1
+    }
+        return ADSdue
+    }
+    
+    
+
+
+
+
+
+
+//write a function to compute LBTT for commercial property purchase
+
+//write another main function to compute LBTT for commercial lease transaction
+
+
+
--- lbtt-calculator/test/lbttCalculator.test.js
+++ lbtt-calculator/test/lbttCalculator.test.js
@@ -1,38 +1,27 @@
-import { lbttCalculator } from "../src/lbttCalculator";
+import { lbttCalculator , ADS, lbttBandCategory } from "../src/lbttCalculator";
 import {test, expect} from "@jest/globals"; 
 
-//test for LBTT on residential property transaction 
-//parameters to consider:  
-    // date of transaction
-    // property type
-    // purchase price
-    // isADSapply
-    // isFirstTimeBuyer
-    // isLinkedTransaction
+//----test for LBTT on residential property transaction -----//
 
+// --test for main lbttCalculator function--//
 //test to cover the following scenarios:
-    // residential property purchase before April 2015 - LBTT not applicable
-    // residential property purchased on 1 April 2021 onwards: 
-        // price 145000 or less - 0% LBTT
-        // price between £145,001 to £250,000 - 2% LBTT
-        // price between  £250,001 to £325,000 - 5% LBTT
-        // price between £325,001 to £750,000 - 10% LBTT
-        // price over £750,000  - 12% LBTT
-
-        // first time buyer relief: 
-            // residential property price £175,000 or less - 0% LBTT
+    // residential property purchase before 1 April 2015 - LBTT not applicable
+         // return -1 (or error message on the date)    
     
-    // residential property purchased between 15 July 2020 to 31 March 2021 (inclusive):
-        //price £250,000 or less - 0% LBTT
-
-    // residential property purchased before 15 July 2020:
-        // same rate as 1 April 2021 onwards, but without first time buyer relief
-
+    // residential property purchased on 1 April 2021 onwards OR Before 15 July 2020
+        // include case with first time buyer relief: 
+        // test for first time buyer with transaction <= £175000, transaction on 1 April 2021 onwards
+            // ----case: transaction on 4/12/2021 with purchase price of £175000 should return LBTT of £600, first time buyer relief of £600 and totalTax of 0 ✅
+        // test for first time buyer with transaction > £175000, transaction on 1 April 2021 onwards
+            // ----case: transaction on 4/12/2021 with purchase price of £200000 should return LBTT of $1100, first time buyer relief of £600 and totalTax of £500 ✅
+        //note: first time buyer relief cannot be applied if ADS applies
+    
+    // residential property purchased between 15 July 2020 and before 1 April 2021
+            // ----case: transaction on 4/12/2020 with purchase price of 175000 should return LBTT of 0 (first time buyer relief is 0 even if it applies) ✅
+        
     // with ADS apply:
-        // transaction on or after 16 December 2022 - 6% of the purchase price
-        // transaction on or after 25 January 2019 - 4% of the purchase price
-        // transaction prior to 25 January 2019 - 3% of the purchase price
-
+        //----case: property price of 175000 purchase on 17/12/2022 with ADS on £150000 should return LBTT of £600 and ADS of £9000 and totalTax of £9600 ✅
+        //----case: property price of 175000 purchased on 4/11/2022 with ADS on 150000 should return LBTT of £600 and ADS of £6000 and totalTax of £6600 ✅
 
 
 // test case example
@@ -42,13 +31,182 @@ import {test, expect} from "@jest/globals";
     // buyer is not first time buyer
 
 
-test('initial test functin to return 0', ()=>{
-    // Arrange - set up the data
-    const propertyPrice = 0;
-    // Act - call the function
-    const result = lbttCalculator(propertyPrice)
-    // Assert 
-    const expected = 0
-    expect(result).toBe(expected)
+//--test for subfunctions used within the main lbttcaltulator function --//
+            
+// test the lbttResidential function
+    // use forEach to test for each band 
+    // "purchase price of %s should return value of %s"
 
-} )
+    // example test cases: 
+        //LBTT should be 0 for price < 145000
+        //LBTT between 145000 and 250000
+        //LBTT between 250000 and 325000
+        //LBTT between 325000 and 750000
+        //LBTT for price above 750000
+
+
+// test for ADS function  
+        // the amount of ADS must be equal or less than the property purchase price
+        // test for ADS with transaction on or after 16 December 2022
+            //case: property purchase on 17/12/2022 with ADS on £150000 should return ADS of £9000
+        // test for ADS with transaction on or after 25 January 2019
+            // case: property purchased on 4/11/2022 with ADS on 150000 should return ADS of £6000
+        // test for ADS with transaction prior to 25 January 2019
+
+
+
+// test for firstTimeBuyerRelief function 
+
+// test for lbttBandCategory function 
+    //-- case: transaction on 1/4/2021 will return BandApril2021
+    //-- case: transaction on 15/7/2020 will return BandJuly2020
+    //-- case: transaction on 14/7/2020 will return BandApril2021
+
+//----------------//
+
+//initial test
+test('initial test function to return 0', ()=>{
+            // Arrange - set up the data
+            const propertyPrice = 0;
+            // Act - call the function
+            const result = lbttCalculator(undefined, propertyPrice, undefined, undefined, undefined)
+            // Assert 
+            const expected = 0
+            expect(result).toBe(expected)
+        
+    } )
+
+//----------------//
+//test for lbttCalculator function
+
+
+//----------------//
+
+
+//test for lbttResidential function
+
+// define array of test cases 
+// the array below are specifically for lbttResidential function,
+// assume first time buyer relief and ADS do not apply.
+
+// tax amount for price less than 145000
+const testCasesA = [[0, 0],
+                    [100000, 0],
+                    [145000, 0]]
+//tax amount for price between 145001 and 250000
+const testCasesB = [[146000, 20],
+                    [200000, 1100],
+                    [250000, 2100]]
+
+// tax amount for price between 250001 and 325000
+const testCasesC = [[260000, 2600],
+                    [300000, 4600],
+                    [325000, 5850]]
+
+// tax amount for price between 325001 and 750000
+const testCasesD = [[326000, 5950], 
+                    [500000, 23350],
+                    [750000, 48350]]
+
+// tax amount for price above 750000
+const testCasesE = [[800000, 54350],
+                    [1000000, 78350]]
+
+                
+//funtion lbttCalculator input argument: (transactionDate, purchasePrice, isADSapply, otherPropertyPrice, isFirstTimeBuyer)
+//function return value: {LBTT: number, 
+                       // ADS: number,
+                       // firstTimeBuyerRelief : number, 
+                        //totalTax:number}
+
+//test code for main function 
+describe("tests for lbttCalculator function", ()=>{
+    test.each(testCasesA)("LBTT for price < 145000. Price of %s should return LBTT of %s", (propertyPrice, expectedLBTT) =>{
+        // const result = lbttCalculator("2022-11-4", propertyPrice, undefined, undefined, undefined)
+        // const lbttResult = result.LBTT
+        // expect(lbttResult).toBe(expectedLBTT)
+    } )
+
+    test.each(testCasesB)("LBTT for price between 145001 and 250000. Price of %s should return LBTT of %s", (propertyPrice, expectedLBTT) =>{
+        //Arrange - specify the transactionDate to be "2022-11-4", and leave other parameters as undefined
+        //Act - call the function
+        //Assert - expect actual results to be expected results. 
+    })
+
+    test.each(testCasesC)("LBTT for price between 250001 and 325000. Price of %s should return LBTT of %s", (propertyPrice, expectedLBTT)=>{
+        //Arrange - specify the transactionDate to be "2022-11-4"
+       
+    })
+
+    test.each(testCasesD)("LBTT for price between 325001 and 750000. Price of %s should return LBTT of %s", (propertyPrice, expectedLBTT)=>{
+        //specify transactionDate to be "2022-11-4"
+       
+    })
+
+    test.each(testCasesE)("LBTT for price above 750000. Price of %s should return LBTT of %s", (propertyPrice, expectedLBTT)=>{
+        //specify transactionDate to be "2022-11-4"
+       
+    })
+})
+
+
+//----test ADS function ----//
+
+//test cases
+const testCasesADS2 = [["2022-12-17", 150000, 9000],
+                    ["2022-11-4", 150000, 6000]]
+
+describe("tests for ADS function", ()=>{
+    test.each(testCasesADS2)("case with ADS: transaction on %s with other property price of %s should result with ADS of %s", (date, otherPropertyPrice, expectedADS)=>{
+        const result = ADS(date, otherPropertyPrice)
+        expect(result).toBe(expectedADS)
+    })
+})
+
+
+//----test cases for first time buyer relief------//
+//this array represents [date, propertyPrice, LBTT, firstTimeBuyerRelief, totalTax]
+const testCasesFTB = [["2021-12-04", 175000, 600, 600, 0],
+                    ["2021-12-04", 200000, 1100, 600, 500]]
+
+//----test cases for ADS------//
+//this array represents [date, propertyPrice, otherPropertyPrice, LBTT, ADS, totalTax]
+const testCasesADS = [["2022-12-17", 175000, 150000, 600, 9000, 9600],
+                    ["2022-11-4", 175000, 150000, 600, 6000, 6600]]
+
+
+//funtion lbttCalculator input argument: (transactionDate, purchasePrice, isADSapply, otherPropertyPrice, isFirstTimeBuyer)
+//function return value: {LBTT: number, 
+                       // ADS: number,
+                       // firstTimeBuyerRelief : number, 
+                        //totalTax:number}
+
+
+
+
+
+//------test for transaction between 15 July2020 and before 1 April 2021----//
+
+// test("property purchased between 15 July 2020 and before 1 April 2021", ()=>{
+//     const result = lbttCalculator("2020-12-04", 175000, undefined, undefined, undefined)
+//     const expected = {LBTT: 0, ADS: 0, firstTimeBuyerRelief: 0, totalTax: 0}
+//     expect(result).toEqual(expected)
+
+// })
+
+
+
+
+//------------------//
+
+                    
+//--other tests to be added--//
+// test for error handling
+        // test if function will throw error if input is invalid 
+            // case of invalid input: propertyPrice is not a number, propertyPrice is negative
+    
+
+// test for different property type
+// test for accuracy up to two decimal points
+
+// test the other sub functions that computes LBTT in different categories. 
